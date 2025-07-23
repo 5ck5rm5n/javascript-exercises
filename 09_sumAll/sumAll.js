c9066d3b682c6b2257583258616d04972e409e36
Paul <184975155+5ck5rm5n@users.noreply.github.com>
2025-07-23 23:51:42 +0800

    Completed Excercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if(typeof a !== 'number' || typeof b !== 'number' 
+                             || a < 0 || b < 0 
+                             || !Number.isInteger(a) 
+                             || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+ 
+    const start = Math.min(a,b);
+    const end = Math.max(a,b);
+    let sum = 0;
+
+    for(i = start; i <= end; i++){
+        sum += i;
+    }
+    if(a < 0 || b < 0){
+        return 'ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
